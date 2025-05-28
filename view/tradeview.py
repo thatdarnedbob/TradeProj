b7ad08e47930f2cdc1832a360bc8ab198316288a
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-27 23:33:12 -0500

    more pygame principles

--- view/tradeview.py
+++ view/tradeview.py
@@ -5,20 +5,31 @@ class TradeView():
         pg.init()
 
         self.screen = pg.display.set_mode((self._screen_width, self._screen_height), vsync=1)
-        self.screen.fill("black")
 
         pg.display.set_caption("The Many Waters of Boolea")
         pg.mouse.set_visible(False)
 
+        self.bg = pg.Surface(self.screen.get_size())
+        self.bg = self.bg.convert()
+        self.bg.fill("black")
+
+        font = pg.font.Font(None, 36)
+        text = font.render("Hello There", 1, "green")
+        textpos = text.get_rect()
+        textpos.centerx = self.bg.get_rect().centerx
+        self.bg.blit(text, textpos)
+
+        self.screen.blit(self.bg, (0,0))
         pg.display.flip()
 
     def dispWhite(self):
-        self.screen.fill("white")
+        self.bg.fill("white")
     
     def dispBlack(self):
-        self.screen.fill("black")
+        self.bg.fill("black")
 
     def refresh(self):
+        self.screen.blit(self.bg, (0,0))
         pg.display.flip()
 
     def __init__(self, width, height):
