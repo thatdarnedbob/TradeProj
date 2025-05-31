7275026e01430c826ee41d509c5abf937aaaf17e
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-30 23:38:10 -0500

    Better menu logic

--- model/trademodel.py
+++ model/trademodel.py
@@ -1,23 +1,65 @@
 from view.tradeview import TradeView
 from enum import Enum
+import gc
 
 class TradeModel():
     # for scope limitation, let's have the TradeModel class handle its own menu logic.
     def __init__(self, width, height, exit_command):
         self._view = TradeView(width, height)
-        self.current_menu = ["Default string, none"]
-        self.menu_selection = 0
+        self.current_menu = None
+        self.menu_actions = None
+        self.menu_selection = [0]
+
         self._exit_command = exit_command
 
     def setup(self):
         self._view.setup()
         self.createMainMenu()
-        self._view.updateMenu(self.current_menu, self.menu_selection)
 
     def createMainMenu(self):
-        self.current_menu = ["Do nothing", "Do less", "Goods", "Exit"]
-        self.menu_actions = [None, None, None, self._exit_command]
-        self.menu_selection = 0
+        self.current_menu = ["Towns", "Ships", "Goods", "Exit"]
+        self.menu_actions = [self.createTownsMenu, self.createShipsMenu, self.createGoodsMenu, self._exit_command]
+        self._view.updateMenu(self.current_menu, self.menu_selection[-1])
+
+    def backToMainMenu(self):
+        del self.current_menu
+        del self.menu_actions
+        gc.collect()
+
+        self.menu_selection.pop()
+        self.current_menu = ["Towns", "Ships", "Goods", "Exit"]
+        self.menu_actions = [self.createTownsMenu, self.createShipsMenu, self.createGoodsMenu, self._exit_command]
+        self._view.updateMenu(self.current_menu, self.menu_selection[-1])
+    
+    def createTownsMenu(self):
+        del self.current_menu
+        del self.menu_actions
+        gc.collect()
+
+        self.current_menu = ["Boolea", "Back to Main Menu"]
+        self.menu_actions = [None, self.backToMainMenu]
+        self.menu_selection.append(0)
+        self._view.updateMenu(self.current_menu, self.menu_selection[-1])
+
+    def createShipsMenu(self):
+        del self.current_menu
+        del self.menu_actions
+        gc.collect()
+
+        self.current_menu = ["River Hauler", "Back to Main Menu"]
+        self.menu_actions = [None, self.backToMainMenu]
+        self.menu_selection.append(0)
+        self._view.updateMenu(self.current_menu, self.menu_selection[-1])
+
+    def createGoodsMenu(self):
+        del self.current_menu
+        del self.menu_actions
+        gc.collect()
+
+        self.current_menu = ["Gold", "Back to Main Menu"]
+        self.menu_actions = [None, self.backToMainMenu]
+        self.menu_selection.append(0)
+        self._view.updateMenu(self.current_menu, self.menu_selection[-1])
 
     def game_step(self):
         pass
@@ -26,14 +68,15 @@ class TradeModel():
         self._view.refresh()
 
     def menu_down(self):
-        if self.menu_selection < len(self.current_menu) - 1:
-            self.menu_selection += 1
-            self._view.updateMenu(self.current_menu, self.menu_selection)
+        if self.menu_selection[-1] < len(self.current_menu) - 1:
+            self.menu_selection[-1] += 1
+            self._view.updateMenu(self.current_menu, self.menu_selection[-1])
     
     def menu_up(self):
-        if self.menu_selection > 0:
-            self.menu_selection -= 1
-            self._view.updateMenu(self.current_menu, self.menu_selection)
+        if self.menu_selection[-1] > 0:
+            self.menu_selection[-1] -= 1
+            self._view.updateMenu(self.current_menu, self.menu_selection[-1])
     
     def execute(self):
-        self.menu_actions[self.menu_selection]()
+        if self.menu_actions[self.menu_selection[-1]]:
+            self.menu_actions[self.menu_selection[-1]]()
